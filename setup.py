50e45552815afb8746775bc44f2738795a3ab1d2
kevin89126 <u3300035@gmail.com>
2016-10-09 22:47:04 +0800

    Fix setup error

--- setup.py
+++ setup.py
@@ -17,5 +17,6 @@ if platform.system() == 'Darwin':
     )
 else:
     from distutils.core import setup
+    import py2exe
     setup(windows=['run.py'],
           data_files = [("img", glob.glob("img\\*"))])
